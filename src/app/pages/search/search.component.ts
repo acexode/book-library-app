040bf02d68ef70d513cbf4c3ea849d06d897abca
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2024-03-06 20:33:52 +0100

    wishlist & search logic

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,12 +1,60 @@
 import { Component } from '@angular/core';
+import { BookService } from '../../services/book.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SearchResponse } from '../../model/search-response.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
-export class SearchComponent {
 
+export class SearchComponent {
+  searchTerm: string = '';
+  searchKey: string = 'title';
+  searchResults: any[] = [];
+  loading: boolean = false;
+  noResult = false;
+  constructor(private bookService: BookService) {}
+
+  search(): void {
+    if (!this.searchTerm.trim()) {
+      // If the search term is empty, clear the search results
+      this.searchResults = [];
+      return;
+    }
+
+    this.loading = true
+    this.bookService.searchBooks(this.searchTerm, this.searchKey).subscribe(
+      (results: SearchResponse) => {
+        console.log(results);
+        this.loading = false;
+        // Limit the search results to the first 9 books
+        if(results.numFound > 0){
+          this.noResult = false
+          this.searchResults = results.docs.map(doc =>{
+            return {
+              id: doc.key.split('/')[2],
+              title: doc.title,
+              publishDate: doc.first_publish_year,
+              authors: doc.author_name[0],
+              cover: doc.cover_i ? `https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg` : null
+            };
+          })
+
+        }else{
+          this.noResult = true
+          this.loading = false
+        }
+      },
+      (error) => {
+        console.error('Error searching for books:', error);
+        // Handle error cases here
+      }
+    );
+  }
 }
--- src/app/pages/wishlist/wishlist.component.ts
+++ src/app/pages/wishlist/wishlist.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { BookStorageService } from '../../services/book-storage.service';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.scss'
 })
-export class WishlistComponent {
+export class WishlistComponent implements OnInit {
+  financeBooks: any[] = [];
+  isMenuOpen: boolean = false;
+  isMobileView: boolean = false;
+  constructor(private store: BookStorageService) {}
 
+  ngOnInit(): void {
+   this.financeBooks = this.store.getBooks()
+  }
+
+
+  removeFromWishList(book: any) {
+   this.store.removeBook(book.id)
+   this.financeBooks = this.store.getBooks()
+  }
 }
